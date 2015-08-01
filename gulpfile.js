e5e1070168dac18af77348172b9d7944c764b8fa
Tiago Resende <tiagor87@gmail.com>
2015-08-01 20:20:42 -0300

    Ajusta compilação do módulo

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,29 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var coveralls = require('gulp-coveralls');
 var cssmin = require('gulp-cssmin');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var karma = require('karma').server;
 
 var _coverage = 'coverage/**/lcov.info';
-var _app = 'src/n4modal.js';
-var _css = 'src/n4modal.css';
-var _appMin = 'n4modal.min.js';
+var _scripts = 'src/**/*.js';
+var _styles = 'src/**/*.css';
+var _scriptMin = 'n4modal.min.js';
+var _styleMin = 'n4modal.min.css';
 var _dist = 'dist';
 
 gulp.task('build-css', function () {
-  return gulp.src(_css)
+  return gulp.src(_styles)
+    .pipe(concat(_styleMin))
     .pipe(cssmin())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(_dist));
 })
 
-gulp.task('build', ['unit_test', 'build-css'], function()
-{
-    gulp
-        .src(_app)
-        .pipe(uglify())
-        .pipe(rename(_appMin))
-        .pipe(gulp.dest(_dist));
+gulp.task('build', ['unit_test', 'build-css'], function () {
+  return gulp.src(_scripts)
+    .pipe(concat(_scriptMin))
+    .pipe(uglify())
+    .pipe(gulp.dest(_dist));
 })
 
 gulp.task('unit_test', function(done)
