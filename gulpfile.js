b1e658a2b7eb05de6d0d637266aaa6ba055eef9b
Tiago Resende <tiagor87@gmail.com>
2015-08-03 19:56:42 -0300

    Gera arquivos não minificados também

--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,40 @@ var karma = require('karma').server;
 var _coverage = 'coverage/**/lcov.info';
 var _scripts = 'src/**/*.js';
 var _styles = 'src/**/*.css';
-var _scriptMin = 'n4modal.min.js';
-var _styleMin = 'n4modal.min.css';
+var _script = 'n4modal.js';
+var _style = 'n4modal.css';
 var _dist = 'dist';
 
 gulp.task('build-css', function () {
   return gulp.src(_styles)
-    .pipe(concat(_styleMin))
+    .pipe(concat(_style))
+    .pipe(gulp.dest(_dist))
     .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(_dist));
 })
 
 gulp.task('build', ['unit_test', 'build-css'], function () {
   return gulp.src(_scripts)
-    .pipe(concat(_scriptMin))
+    .pipe(concat(_script))
+    .pipe(gulp.dest(_dist))
     .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(_dist));
 })
 
-gulp.task('unit_test', function(done)
-{
-    var _opts = {
-                  configFile: __dirname + '/karma.conf.js',
-                  singleRun: true,
-                  browsers: ['PhantomJS']
-               };
+gulp.task('unit_test', function (done) {
+  var _opts = {
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+    browsers: ['PhantomJS']
+  };
 
-    karma.start(_opts, done);
+  karma.start(_opts, done);
 })
 
-gulp.task('coverage', ['unit_test'], function()
-{
-    gulp
-        .src(_coverage)
-        .pipe(coveralls());
+gulp.task('coverage', ['unit_test'], function () {
+  gulp
+    .src(_coverage)
+    .pipe(coveralls());
 })
